0766eab4bebc87ca661ecd6a371ded9f10dceffc
Temperz87 <58375877+Temperz87@users.noreply.github.com>
2022-09-09 11:30:54 -0400

    I forgor
    to commit this

---
+++ Ultrakill shhhhh/ThunderGun.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ThundergunShockWave : MonoBehaviour
+{
+    private List<Collider> hitColliders = new List<Collider>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.CheckCollision(collision.collider);
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        this.CheckCollision(collision);
+    }
+    private void CheckCollision(Collider col)
+    {
+        if (col.gameObject.layer == 10)
+        {
+            EnemyIdentifierIdentifier enemyIdentifierIdentifier = col.gameObject.GetComponent<EnemyIdentifierIdentifier>();
+            if (enemyIdentifierIdentifier != null && enemyIdentifierIdentifier.eid != null)
+            {
+                Rigidbody body = enemyIdentifierIdentifier.GetComponentInChildren<Rigidbody>(true);
+                if (!hitColliders.Contains(col) && body)
+                {
+                    hitColliders.Add(col);
+                    body.AddForceAtPosition(new Vector3(base.transform.position.x - enemyIdentifierIdentifier.transform.position.x, 0f, base.transform.position.z - enemyIdentifierIdentifier.transform.position.z) * 999f, col.transform.position);
+                }
+            }
+        }
+        return;
+    }
+}
